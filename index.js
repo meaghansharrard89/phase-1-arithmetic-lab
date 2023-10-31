54aeed6c5c4b2832f4cbeeccc1212bc5820a29db
Meaghan <meaghan.sharrard89@gmail.com>
2023-10-30 19:14:45 -0700

    passing tests

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+var multiply = num1 * num2;
 
-// Write your code here
+var random = Math.ceil(Math.random() * 10);
+
+let num3 = 22;
+let num4 = 9;
+var mod = num3 % num4;
+
+var max = Math.round(19.5);
